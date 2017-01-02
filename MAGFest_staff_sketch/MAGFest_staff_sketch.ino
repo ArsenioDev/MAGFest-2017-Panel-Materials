d3e0762c95b6346053f6676f36cc1d1c0191b9d9
ArsenioDev <ArsenioDev@users.noreply.github.com>
2017-01-02 00:29:17 -0500

    Added info and license

--- MAGFest_staff_sketch/MAGFest_staff_sketch.ino
+++ MAGFest_staff_sketch/MAGFest_staff_sketch.ino
@@ -1,6 +1,13 @@
-
-
-
+/*Wizard staff code for MAGFest 2017 panel
+Written by Arsenio Dev
+Used in "Lights, Motion, Sound: Adding programmability to cosplay with Arduino"
+* ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Arsenio Dev wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. 
+ * ----------------------------------------------------------------------------
+ */
 
 //Passive activity is breathing LEDs, on trigger or fire cue strobe firing
 
