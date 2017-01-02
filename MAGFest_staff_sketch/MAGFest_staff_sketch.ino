3a0309371ceff767bd344dcce3df3409a01738af
ArsenioDev <arsenio.dev@gmail.com>
2017-01-01 22:39:45 -0500

    slight modifications

--- MAGFest_staff_sketch/MAGFest_staff_sketch.ino
+++ MAGFest_staff_sketch/MAGFest_staff_sketch.ino
@@ -2,10 +2,9 @@
 
 #include <WS2812FX.h>
 
-#define LED_COUNT 13
-#define LED_PIN 11
-
-#define TIMER_MS 5000
+#define LED_COUNT 5
+#define LED_PIN 8
+#define TRIGGER 2
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -15,19 +14,50 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
+WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+bool oldState = HIGH;
+int showType = 0;
 
 void setup() {
   // put your setup code here, to run once:
   ws2812fx.init();
   ws2812fx.setBrightness(255);
-  ws2812fx.setSpeed(200);
-  ws2812fx.setColor(0x007BFF);
-  ws2812fx.setMode(FX_MODE_MULTI_STROBE);
+  ws2812fx.setColor(0xFFFFFF);
+  ws2812fx.setMode(FX_MODE_BREATH);
   ws2812fx.start();
+  pinMode(TRIGGER, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ws2812fx.service();
 
+ // Get current button state.
+digitalRead(TRIGGER);
+ bool newState = digitalRead(TRIGGER);
+
+  // Check if state changed from high to low (button press).
+  if (newState == LOW && oldState == HIGH) {
+    // Short delay to debounce button.
+    delay(20);
+    // Check if button is still low after debounce.
+    newState = digitalRead(TRIGGER);
+    if (newState == LOW) {
+     showType++;
+      if (showType > 1)
+        showType=0;
+      startShow(showType);
+    }
+  }
+   // Set the last button state to the old state.
+  oldState = newState;
+}
+
+void startShow(int i) {
+  switch(i){
+    case 0: ws2812fx.setMode(FX_MODE_BREATH);
+            break;
+    case 1: ws2812fx.setMode(FX_MODE_MULTI_STROBE);            
+            break;
+
+  }
 }
